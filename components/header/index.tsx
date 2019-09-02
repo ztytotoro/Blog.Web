dbce9826fdcd9b0ac4a5af4c6107d7e78938545e
Yarn <ztytotoro@outlook.com>
2019-09-03 00:46:49 +0800

    update

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -1,10 +1,16 @@
 import React from "react";
+import Link from "next/link";
+
 const css = require("./index.scss");
 
 export const Header: React.FC = () => {
   return (
     <div className={css.header}>
-      <span className={css.headerTitle}>CatLien</span>
+      <span className={css.headerTitle}>
+        <Link href="/">
+          <a>CatLien</a>
+        </Link>
+      </span>
       <div className={css.headerAvator}>
         <span>Y</span>
       </div>
