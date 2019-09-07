99b7200431f985f1ac20bd20bd43be7b7f195900
Yarn <ztytotoro@outlook.com>
2019-09-07 17:35:30 +0800

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,11 @@
-import React from "react";
-import { Layout } from "../components/layout";
-import { Icon } from "office-ui-fabric-react/lib-commonjs/Icon";
+import React from 'react';
+import { Layout } from '../components/layout';
+import { Icon } from 'office-ui-fabric-react/lib-commonjs/Icon';
+import { NextPage } from 'next';
 
-const css = require("./index.scss");
+const css = require('./index.scss');
 
-const Home: React.FC = () => {
+const Home: NextPage = () => {
   return (
     <Layout title="Home">
       <div className={css.body}>
